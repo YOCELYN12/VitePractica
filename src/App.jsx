46fe4467d89ce18a4321e0cff510974c850f0a9a
YOCELYN12 <yocelynrivera9@gmail.com>
2024-06-17 10:30:56 -0600

    commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>Inicio</li>
+          <li>Iniciar </li>
+        </ul>
+      </nav>
+    </>
+  )
+}
+
+export default App
